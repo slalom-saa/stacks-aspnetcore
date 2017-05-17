55cbff98cf329cc50542c894e5096a269b23cd41
George Olson <georgeo@slalom.com>
2017-05-16 20:34:29 -0700

    Updated session to use cookie

--- src/Slalom.Stacks.AspNetCore/Messaging/WebRequestContext.cs
+++ src/Slalom.Stacks.AspNetCore/Messaging/WebRequestContext.cs
@@ -5,6 +5,7 @@
  * the LICENSE file, which is part of this source code package.
  */
 
+using System;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Slalom.Stacks.Services.Messaging;
@@ -23,6 +24,20 @@ namespace Slalom.Stacks.AspNetCore.Messaging
         protected override string GetSourceIPAddress()
         {
             return _accessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+        }
+
+
+        protected override string GetSession()
+        {
+            var context = _accessor.HttpContext;
+            var key = Guid.NewGuid().ToString();
+            if (context.Request.Cookies.ContainsKey("Stacks-Session"))
+            {
+                key = context.Request.Cookies["Stacks-Session"];
+                context.Response.Cookies.Delete("Stacks-Session");
+            }
+            context.Response.Cookies.Append("Stacks-Session", key, new CookieOptions { Expires = DateTimeOffset.Now.AddMinutes(15) });
+            return key;
         }
 
         protected override ClaimsPrincipal GetUser()
