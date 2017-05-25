08772e697f71abb87f3d15f5b931c2f91a4098ad
George Olson <georgeo@slalom.com>
2017-05-25 14:58:33 -0700

    Update to VS 2017

--- assets/Setup.ps1
+++
@@ -1,4 +0,0 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install mongodb -y
-Install-ChocolateyVsixPackage -PackageName "SerilogAnalyzer" `
-  -VsixUrl https://marketplace.visualstudio.com/items?itemName=Suchiman.SerilogAnalyzer
